9f2c9f64489daf48a54eb195e7bb94194b9d021e
abdul ghani <mohammedghani842@gmail.com>
2021-04-01 12:51:35 -0700

    working on quiz app

--- script.js
+++ script.js
@@ -31,23 +31,27 @@ const quizData = [
         c:"1994",
         d:"1995",
         correct:"d"
-    }
-]
+    },
+];
+
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer")
-const answerEl = document.getElementById("question");
+const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text")
 const b_text = document.getElementById("b_text")
 const c_text = document.getElementById("c_text")
 const d_text = document.getElementById("d_text")
 const submitBtn = document.getElementById("submit");
+let scores = document.getElementById("score")
+
  
 let currentQuiz = 0;
 let score = 0;
 loadQuiz();
 function loadQuiz(){
+    deselectAnswer();
     const currentQuizData = quizData[currentQuiz]
-    answerEl.innerText = currentQuizData.question;
+    questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
@@ -57,9 +61,40 @@ function loadQuiz(){
     function getSelected() {
         let answer = undefined;
         answerEls.forEach((answerEl) => {
-            console.log(answerEl)
+            
+
+
+            
             if(answerEl.checked) {
                 answer = answerEl.id;
             }
         });
+        return answer;
     }
+    function deselectAnswer(){
+        answerEls.forEach((answerEl)=>{
+           answerEl.checked=false;
+        });
+    }
+    submitBtn.addEventListener("click", ()=>{
+        const answer = getSelected();
+
+        if(answer){
+            if(answer === quizData[currentQuiz].correct){
+             score++;
+             
+            }
+            currentQuiz++;
+            if(currentQuiz<quizData.length){
+                loadQuiz();
+            }else{
+                
+                quiz.innerHTML=`<h2> your score is ${score}/${quizData.length} this
+                <button onclick="location.reload()">Reload</button>`;
+                
+            }
+        }
+    });
+   
+    
+    
